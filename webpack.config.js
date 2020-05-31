32aad4708117ac3b67b78b4c23e99e08a1e037e9
Gang Zhou <jasonzhou@Gangs-MacBook-Pro.local>
2020-05-31 15:27:56 +0800

    add babel support

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,19 @@
 module.exports = {
     // 1
     entry: './src/index.js',
+    //
+    module: {
+      rules: [
+        {
+          test: /\.(js)$/, 
+          exclude: /node_modules/,
+          use: ['babel-loader']
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['*', '.js']
+    },
     // 2
     output: {
       path: __dirname + '/dist',
